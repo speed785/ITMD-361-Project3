d8d622d1aa2f65e462639307e7152888532e7fd5
James <ioandumitru59@gmail.com>
2019-04-27 07:59:57 -0500

    Made the google map work
    Used to only show circles, now shows the map correctly.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.8813, lng: 87.7148},
-    zoom: 20
+    zoom: 5
   });
 }
 /*Javscript for Google Map*/
