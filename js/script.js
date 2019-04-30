5c3f41ca3ebe1579356389568e0be92aad59d589
James <ioandumitru59@gmail.com>
2019-04-30 08:46:37 -0500

    Updated all files
    Added two more photos for slider, made the map work to show the correct coordinates, made changes to the text, centered the nav, added a slider with buttons on the bottom. created the pages to be scalable.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.8813, lng: 87.7148},
-    zoom: 5
+    center: {lat: 41.8862729, lng: -87.7172554},
+    zoom: 18
   });
 }
 /*Javscript for Google Map*/
